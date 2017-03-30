fe8d889a3b1a37f29f620dc671ebc53336731622
kirkward15 <kiward@student.csdspartans.org>
2017-03-30 09:25:25 -0400

    Update readprintpoemwithcounters.py

--- printtest/readprintpoemwithcounters.py
+++ printtest/readprintpoemwithcounters.py
@@ -1,33 +1,44 @@
-#   This reads and prints the whole document.
-#           It does not prt a blank line after each line of the file
+name_of_mydocument = 'tuesdayafternoon.txt'
+file_input = open("tuesdayafternoon.txt", 'r')    #open file for reading
 
-name_of_mydocument = 'mydocument.txt'                # Name the file
-file_input = open(name_of_mydocument, 'r')           # open file for reading
+line = file_input.readline()
+print (line, end = '')
+line = file_input.readline()
+print (line, end = '')
+line = file_input.readline()
+print (line, end = '')
+line = file_input.readline()
 
-line = file_input.readline()                         # read and print title
-print(line)
-line = file_input.readline()                         # read and print author
-print(line)
-line = file_input.readline()                         # read and print blank line
-print(line)
-
-line = file_input.readline()                         # read first line of actual poem
-
-line_counter = 0                                     # initialize counters
+line_counter = 1 
 stanza_counter = 1
+total_number_of_lines_in_file = 3
 
-while line != '':                                     # while not end of file
+while line != '':                      # while not end of file
+    if line == '\n':
+      stanza_counter += 1 
+      print(" ") 
+    else:
+      if line_counter < 10:
+        
+        print(line_counter,") ",line, end = '')              # don't print another new line
+        line_counter += 1
+      else:
+        print(line_counter,")",line, end = '')              # don't print another new line
+        line_counter += 1
+    line = file_input.readline()
+    total_number_of_lines_in_file += 1
 
-    if line == '\n':                                    # is line a blank line - which indicates a new stanza
-      stanza_counter += 1                                   # increment stanza counter  
-      print ()                                              # print blank line
-    else:                                               # line is not a blank line
-      line_counter += 1                                    # increment line counter
-      print(line_counter, line, end = '')                  # print line counter and line, prevent printing another new line
-    line = file_input.readline()                        # read next line in file
-    
-print ()                                               # print blank line
-print ()                         
-print ("The number of stanzas is ",  stanza_counter)   # print stanza counter
 
-file_input.close()                                     # close file
+
+print(" ")
+print(" ") 
+print("The number of lines in the poem is:",line_counter - 1)
+print("The number of stanzas in the poem is:", stanza_counter)
+
+
+
+print("The total number of lines in the file is:", total_number_of_lines_in_file)
+file_input.close() 
+
+print("The song \"Tuesday Afternoon\" first appeared on the album \033[3mDays of Future Passed\033[0m in 1967.")
+print("The members of the band are Graeme Edge, Justin Hayward, John Lodge, Ray Thomas, Mike Pinder.")
